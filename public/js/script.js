3fc5c65c80cc08a3f0a202fbbfcb1f392d7645c8
jmay1 <jmay1@terpmail.umd.edu>
2020-01-23 22:06:20 -0500

    Added navigation feature

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
